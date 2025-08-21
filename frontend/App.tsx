2dda9c972f79f102d8971436f747dccccceb7be5
ahmedmoshendidi <ahmedmoalshendidi@gmail.com>
2025-08-21 22:21:10 +0200

    add import index.css

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Button } from './components/ui/button';
 import { Card, CardContent } from './components/ui/card';
 import 'flag-icons/css/flag-icons.min.css';
+import 'index.css';
 
 import { ImageWithFallback } from './components/figma/ImageWithFallback';
 import TourDetails from './components/TourDetails';
