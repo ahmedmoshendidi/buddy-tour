3705918fb870fe655d39d16dbe32a9782d3f9dc7
ahmedmoshendidi <ahmedmoalshendidi@gmail.com>
2025-09-09 01:21:46 +0200

    fix(email): correct nodemailer API call createTransporter -> createTransport

--- utils/emailService.js
+++ utils/emailService.js
@@ -9,7 +9,7 @@ class EmailService {
   initializeTransporter() {
     // Create transporter based on environment
     if (process.env.EMAIL_SERVICE === 'gmail') {
-      this.transporter = nodemailer.createTransporter({
+      this.transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
           user: process.env.EMAIL_USER,
@@ -17,7 +17,7 @@ class EmailService {
         }
       });
     } else if (process.env.SMTP_HOST) {
-      this.transporter = nodemailer.createTransporter({
+      this.transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT || 587,
         secure: process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
